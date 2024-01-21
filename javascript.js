2ff57c336c2e4884f4c4b868ebcdb626bbb39323
Leo1nardo1 <leonardo.carvalho.ferreira@protonmail.com>
2024-01-21 16:13:13 -0300

    leoflix

---
+++ javascript.js
@@ -0,0 +1,37 @@
+const formPesquisa = document.querySelector('form');
+
+//O formulário tem um evento onsubmit, que recebe uma função, neste caso uma arrow function.
+formPesquisa.onsubmit = (ev) => {
+  //Deixa de executar o comportamento padrão do evento.
+  ev.preventDefault();
+  
+  //target é o form e dentro do form temos um campo com name = pesquisa
+  const pesquisa = ev.target.pesquisa.value;
+  const apiKey = "2d604fb";
+  
+  //Realiza a validação do campo.
+  if(pesquisa ==""){
+    alert("Preencha o campo!");
+    //caso não tenha informação ele não prossegue
+    return;
+  }
+
+  //result é um parâmetro de uma função e o retorno dessa função é a conversão do result para json
+  fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${pesquisa}`).then(result => result.json()).then(json => carregaLista(json));
+}
+
+const carregaLista = (json) => {
+  const lista = document.querySelector("div.lista");
+  lista.innerHTML = "";
+
+  json.Search.forEach(element => {
+    console.log(element);
+
+    let item = document.createElement("div");
+    item.classList.add("item");
+
+    item.innerHTML = `<img src="${element.Poster}"/><h2>${element.Title}</h2>`;
+
+    lista.appendChild(item);
+  })
+}
